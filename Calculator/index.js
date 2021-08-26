c8c16a31c26ab2d66f80ff38697315cabdd80876
LucasBilbao <lucasbm789@gmail.com>
2021-08-26 12:27:56 +0400

    fix: problems

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,5 +1,5 @@
 class Calculator{
-  constructor(previousOperandTxt, currentOperandTxt){
+  constructor(previousOperandTxt, currentOperandTxt) {
     this.previousOperandTxt = previousOperandTxt;
     this.currentOperandTxt = currentOperandTxt;
     this.clear();
@@ -12,7 +12,7 @@ class Calculator{
   }
 
   delete() {
-    if(this.currentOperand === '' && this.previousOperand !== ''){
+    if(this.currentOperand === '' && this.previousOperand !== '') {
       this.currentOperand = this.previousOperand;
       this.previousOperand = '';
       this.operation = undefined;
@@ -25,27 +25,29 @@ class Calculator{
     if(
       !(
         num === '.' &&
-        this.currentOperand.includes('.') ||
-        this.operation === '√'
-      )){
+        this.currentOperand.includes('.') 
+      )) {
       this.currentOperand = `${this.currentOperand}${num}`;
     }
   }
 
-  negative(){
-    if(this.currentOperand === '' ){
+  negative() {
+    if(this.currentOperand === '' && this.operation != '√') {
       return this.currentOperand = '-';;
     }
   }
 
   chooseOperation(operation) {
-    if(this.currentOperand === '-'){
+    if(operation === '-') {
+      this.negative();
+    }
+    if(this.currentOperand === '-') {
       return;
     }
-    if(this.previousOperand !== ''){
+    if(this.previousOperand !== '') {
       this.compute();
     }
-    if(this.currentOperand !== ''){
+    if(this.currentOperand !== '') {
       this.operation = operation;
       this.previousOperand = this.currentOperand;
       this.currentOperand = '';
@@ -57,40 +59,13 @@ class Calculator{
     const prev = parseFloat(this.previousOperand);
     const current = parseFloat(this.currentOperand);
 
-    // In Case someone tries to divide by 0
-    if(current === 0 && this.operation === '÷'){
-      this.currentOperand = "Can't divide by 0!";
-      this.operation = undefined;
-      this.previousOperand = '';
-
-      this.updateDisplay();
-
-      this.currentOperand = '';
-      this.operation = undefined;
-      this.previousOperand = '';
-      throw new Error ('Error, division by 0!')
-    }
-
-    // In Case someone tries to take the square root of a negative number
-    if(prev < 0 && this.operation === '√'){
-      this.currentOperand = 'Undefined result!';
-      this.operation = undefined;
-      this.previousOperand = '';
-
-      this.updateDisplay();
-
-      this.currentOperand = '';
-      this.operation = undefined;
-      this.previousOperand = '';
-      throw new Error ('Error, Sqrt from a negative number!')
-    }
+    this.checkError(current, prev);
 
     if
     (
-      !(isNaN(prev) || isNaN(current)) &&
-      !(this.operation === '√' && isNaN(prev))
-    ){
-      switch(this.operation){
+      !(isNaN(prev) || isNaN(current))
+    ) {
+      switch(this.operation) {
         case '+':
           computation = prev + current
           break
@@ -109,17 +84,50 @@ class Calculator{
         default: return
       }
     }
-
-    // for some reason switch does not work with '√' so I wrote an extra if
-    if(this.operation === '√'){
+    
+    // for some reason switch does not work with '√' so I wrote an extra "if"
+    if(this.operation === '√') {
       computation = prev ** (1/2)
     }
+
     this.currentOperand = this.resultLengthCheck(computation.toString());
     this.operation = undefined;
     this.previousOperand = '';
   }
 
-  resultLengthCheck(str){
+  checkError(current, prev) {
+    // In Case someone tries to divide by 0
+    if(current === 0 && this.operation === '÷') {
+      this.currentOperand = "Can't divide by 0";
+      this.operation = undefined;
+      this.previousOperand = '';
+
+      this.updateDisplay();
+
+      this.currentOperand = '';
+      this.operation = undefined;
+      this.previousOperand = '';
+      throw new Error ('Error, division by 0!')
+    }
+
+    // In Case someone tries to take the square root of a negative number
+    if(prev < 0 && this.operation === '√') {
+      this.currentOperand = 'Undefined result';
+      this.operation = undefined;
+      this.previousOperand = '';
+
+      this.updateDisplay();
+
+      this.currentOperand = '';
+      this.operation = undefined;
+      this.previousOperand = '';
+      throw new Error ('Error, Sqrt from a negative number!')
+    }
+  }
+
+  resultLengthCheck(str) {
+    str = this.convertToLong(str);
+
     // count how many digits there are in the result
     const obj = str.match(/[0-9]/g);
     let size = 0;
@@ -128,19 +136,44 @@ class Calculator{
         if (obj.hasOwnProperty(k)) size++;
     }
 
+    return this.round(str, size);
+  }
+
+  convertToLong(str) {
+    // convert from compact 
+    const l = str.length;
+    const e = str.indexOf('e', 0);
+
+    if(e !== -1) {
+      const power = parseFloat(str.slice(e + 1));
+
+      str = str.slice(0, e);
+      if(power >= 10) {
+        str = (parseFloat(str) * (10 ** 10)).toString();
+      }else if(power <= -10) {
+        str = (parseFloat(str) * (10 ** (-10))).toString();
+      }else {
+        str = (parseFloat(str) * (10 ** power)).toString();
+      }
+    }
+
+    return str;
+  }
+
+  round(str, size) {
     // Round down to 10 digits if necessary
     const commaIndex = str.indexOf('.', 0);
+    const l = str.length;
     let rounded = parseFloat(str);
 
-    if(size <= 10){
+    if(size <= 10) {
       return rounded.toString();
-    }else if(commaIndex === -1){
-      rounded = Math.round(rounded * (10 ** (10 - str.length))); 
-      rounded /= (10 ** (10 - str.length));
+    }else if(commaIndex === -1) {
+      rounded = Math.round(rounded * (10 ** (10 - l)));
       return rounded.toString();
-    }else if(commaIndex === 10){
+    }else if(commaIndex === 10) {
       return Math.round(rounded).toString();
-    }else if(commaIndex < 10){
+    }else if(commaIndex < 10) {
       rounded = Math.round(rounded * (10 ** (10 - commaIndex)));
       rounded /= (10 ** (10 - commaIndex));
       return rounded.toString();
@@ -155,11 +188,11 @@ class Calculator{
 
     this.currentOperandTxt.innerHTML = this.currentOperand;
 
-    if(this.operation != null){
+    if(this.operation != null) {
       this.previousOperandTxt.innerHTML = (
         `${this.previousOperand} ${this.operation}`
       );
-    }else if(this.previousOperand === ''){
+    }else if(this.previousOperand === '') {
       this.previousOperandTxt.innerHTML = this.previousOperand;
     }
   }
@@ -180,11 +213,6 @@ const minusBtn = document.querySelector("[data-minus]");
 const calculator = new Calculator(previousOperandTxt, currentOperandTxt);
 
 
-minusBtn.addEventListener("click", () => {
-  calculator.negative();
-  calculator.updateDisplay();
-})
-
 numberBtn.forEach(btn => {
   btn.addEventListener("click", () => {
     calculator.appendNum(btn.innerHTML);
@@ -198,7 +226,6 @@ operationBtn.forEach(btn => {
     calculator.updateDisplay();
   })
 })
-
 
 clearBtn.addEventListener("click", () => {
   calculator.clear();
